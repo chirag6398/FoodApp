4e0f31d2379d26301b05cf4061a48beda6e39840
chirag6398 <agarwalchirag112@gmail.com>
2023-03-05 00:10:23 +0530

    added outlet agent page

--- frontend/controller/outletAgent.js
+++ frontend/controller/outletAgent.js
@@ -3,171 +3,179 @@
 ///<reference path="../services/cart.service.js"/>
 
 function generateOrderId() {
-    const timestamp = new Date().getTime(); 
-    const randomNumber = Math.floor(Math.random() * 10000); 
-    const orderId = "ORDERID-"+timestamp+"-"+randomNumber; 
-    return orderId;
+  const timestamp = new Date().getTime();
+  const randomNumber = Math.floor(Math.random() * 10000);
+  const orderId = "ORDERID-" + timestamp + "-" + randomNumber;
+  return orderId;
 }
 
-app.controller("outletAgentController",["$scope","$http","$location","outletAgentApi","cartService","$interval","$rootScope",function($scope,$http,$location,outletAgentApi,cartService,$interval,$rootScope){
+app.controller("outletAgentController", [
+  "$scope",
+  "$http",
+  "$location",
+  "outletAgentApi",
+  "cartService",
+  "$interval",
+  "$rootScope",
+  function (
+    $scope,
+    $http,
+    $location,
+    outletAgentApi,
+    cartService,
+    $interval,
+    $rootScope
+  ) {
     outletAgentApi.getOutletAgentPage();
-    
-    $rootScope.$on('passData',function(err,result){
-        if(result){
 
-            console.log(result)
-            $scope.brand=result.data.brand;
-            $scope.outlet=result.data.outlet;
-            $scope.outletId=result.data.outlet._id;
-            $scope.admin={
-                userName:result.data.userName,
-                email:result.data.email,
-                firstName:result.data.firstName,
-                lastName:result.data.lastName,
-                number:result.data.number
-            }
-            $scope.adminId=result.data._id;
-            
-          
-           
-
-            outletAgentApi.getOutletProducts($scope.outletId);
-            
-            
-        }
-    });
-
-    $interval(function() {
-        $scope.getCurrentTime();
-      }, 1000);
-
-   
-
-    
-    $rootScope.$on('agentProducts',function(err,result){
+    $rootScope.$on("passData", function (err, result) {
+      if (result) {
         console.log(result);
-        $scope.categories=[];
-        $scope.brandLogo="";
-        if(result.data && result.data.length>0){
-            if($scope.brandLogo==""){
-                $scope.brandLogo=result.data[0].brandLogo;
-            }
-            result.data.forEach(function(value){
-            
-                $scope.categoryProducts={[value.name]:value.products};
-                $scope.categories.push(value.name);
-    
-            })
-            console.log($scope.categories,$scope.brandLogo);
-            $scope.isSelected=0;
-            $scope.products=$scope.categoryProducts[$scope.categories[$scope.isSelected]];
-        }
-        
-        
+        $scope.brand = result.data.brand;
+        $scope.outlet = result.data.outlet;
+        $scope.outletId = result.data.outlet._id;
+        $scope.admin = {
+          userName: result.data.userName,
+          email: result.data.email,
+          firstName: result.data.firstName,
+          lastName: result.data.lastName,
+          number: result.data.number,
+        };
+        $scope.adminId = result.data._id;
 
+        outletAgentApi.getOutletProducts($scope.outletId);
+      }
     });
 
-    $rootScope.$on('agentProductsError',function(err,result){
-        console.log(result);  
+    $interval(function () {
+      $scope.getCurrentTime();
+    }, 1000);
+
+    $rootScope.$on("agentProducts", function (err, result) {
+      console.log(result);
+      $scope.categories = [];
+      $scope.brandLogo = "";
+      if (result.data && result.data.length > 0) {
+        if ($scope.brandLogo == "") {
+          $scope.brandLogo = result.data[0].brandLogo;
+        }
+        result.data.forEach(function (value) {
+          //   console.log(value.name);
+          $scope.categoryProducts = {
+            ...$scope.categoryProducts,
+            [value.name]: value.products,
+          };
+          $scope.categories.push(value.name);
+        });
+        // console.log($scope.categories,$scope.brandLogo);
+        $scope.isSelected = 0;
+        $scope.products =
+          $scope.categoryProducts[$scope.categories[$scope.isSelected]];
+        // console.log($scope.categoryProducts);
+        // console.log($scope.categories[$scope.isSelected], $scope.products);
+      }
     });
 
-    $rootScope.$on('notEligible',function(err,isEligible){
-        if(!isEligible){
-            $location.path('login')
-        }
-    })
+    $rootScope.$on("agentProductsError", function (err, result) {
+      console.log(result);
+    });
 
-   
-    
-    $scope.getCurrentTime = function() {
-        $scope.currentTime = new Date();
+    $rootScope.$on("notEligible", function (err, isEligible) {
+      if (!isEligible) {
+        $location.path("login");
+      }
+    });
+
+    $scope.getCurrentTime = function () {
+      $scope.currentTime = new Date();
     };
 
-    $scope.updateAdmin=function ($event,id){
-        console.log(id);
-        outletAgentApi.updateAdmin($scope.admin,id,function(result){
-            console.log(result);
-        })
-    }
+    $scope.updateAdmin = function ($event, id) {
+      console.log(id);
+      outletAgentApi.updateAdmin($scope.admin, id, function (result) {
+        console.log(result);
+      });
+    };
 
-    $scope.changePassword=function($event,id){
-        console.log(id);
-        outletAgentApi.updatePassword($scope.admin,id,function(err,result){
-            if(result){
-                console.log(result)
-            }
-        })
-    }
-
-     
-    $scope.orderNo=generateOrderId();
-    $scope.customer={}
-    $scope.amount=0;
-    $scope.btnText="Enter";
-    $scope.orderBtn="placed order"
-    $scope.saved=false;
-    $scope.object={cart:[]};
-
-    $scope.saveCustomerData=function()
-    {
-        $scope.btnText="saved";
-        $scope.saved=true;
-        console.log($scope.customer);
-    }
-    $scope.addToCart=function(product){
-        
-        
-        $scope.object.cart=cartService.addToCart($scope.object.cart,product);
-
-        $scope.amount=cartService.totalPrice($scope.object.cart);
-        
-        console.log($scope.object.cart)
-        
-        
-    }
-    $scope.plus=function(product){
-        $scope.object.cart=cartService.addToCart($scope.object.cart,product);
-
-        $scope.amount=cartService.totalPrice($scope.object.cart);
-        
-    }
-
-    $scope.minus=function(product){
-        $scope.object.cart=cartService.removeFromCart($scope.object.cart,product);
-
-        $scope.amount=cartService.totalPrice($scope.object.cart);
-        
-    }
-
-    $scope.orderHandler=function(){
-        $scope.orderBtn="placing..."
-        console.log($scope.customer,$scope.object.cart);
-        var data={
-            customer:{
-                name:$scope.customer.name,
-                number:$scope.customer.number
-            },
-            orderId:$scope.orderNo,
-            brand:{
-                _id:$scope.brand._id,
-                name:$scope.brand.name
-            },
-            item:$scope.object.cart,
-            outlet:{
-                _id:$scope.outlet._id,
-                name:$scope.outlet.name
-            }
+    $scope.changePassword = function ($event, id) {
+      console.log(id);
+      outletAgentApi.updatePassword($scope.admin, id, function (err, result) {
+        if (result) {
+          console.log(result);
         }
-        outletAgentApi.placeOrder(data,function(err,result){
-            if(result){
-                alert("order placed");
-                $scope.orderNo=generateOrderId();
-                $scope.object={cart:[]};
-                $scope.amount=0;
-                $scope.saved=false;
-                $scope.customer={};
-            }
-        });
+      });
+    };
 
-    }
-}])
+    $scope.orderNo = generateOrderId();
+    $scope.customer = {};
+    $scope.amount = 0;
+    $scope.btnText = "Enter";
+    $scope.orderBtn = "placed order";
+    $scope.saved = false;
+    $scope.object = { cart: [] };
+
+    $scope.saveCustomerData = function () {
+      $scope.btnText = "saved";
+      $scope.saved = true;
+      console.log($scope.customer);
+    };
+    $scope.addToCart = function (product) {
+      $scope.object.cart = cartService.addToCart($scope.object.cart, product);
+
+      $scope.amount = cartService.totalPrice($scope.object.cart);
+
+      console.log($scope.object.cart);
+    };
+    $scope.plus = function (product) {
+      $scope.object.cart = cartService.addToCart($scope.object.cart, product);
+
+      $scope.amount = cartService.totalPrice($scope.object.cart);
+    };
+
+    $scope.minus = function (product) {
+      $scope.object.cart = cartService.removeFromCart(
+        $scope.object.cart,
+        product
+      );
+
+      $scope.amount = cartService.totalPrice($scope.object.cart);
+    };
+
+    $scope.orderHandler = function () {
+      $scope.orderBtn = "placing...";
+      console.log($scope.customer, $scope.object.cart);
+      var data = {
+        customer: {
+          name: $scope.customer.name,
+          number: $scope.customer.number,
+        },
+        orderId: $scope.orderNo,
+        brand: {
+          _id: $scope.brand._id,
+          name: $scope.brand.name,
+        },
+        item: $scope.object.cart,
+        outlet: {
+          _id: $scope.outlet._id,
+          name: $scope.outlet.name,
+        },
+      };
+      outletAgentApi.placeOrder(data, function (err, result) {
+        if (result) {
+          alert("order placed");
+          $scope.orderNo = generateOrderId();
+          $scope.object = { cart: [] };
+          $scope.amount = 0;
+          $scope.saved = false;
+          $scope.customer = {};
+        }
+      });
+    };
+
+    $scope.setIndexing = function (ind) {
+      $scope.isSelected = ind;
+      $scope.products =
+        $scope.categoryProducts[$scope.categories[$scope.isSelected]];
+    };
+  },
+]);
--- frontend/controller/outletEmployeeForm.js
+++ frontend/controller/outletEmployeeForm.js
@@ -1,40 +1,52 @@
 ///<reference path="../module/module.js"/>
 ///<reference path="../factory/apicall.js"/>
 
-
-
-app.controller("outletEmployeeFormController",["$scope","$http","$location","apiHandler","outletApi","$rootScope",function($scope,$http,$location,apiHandler,outletApi,$rootScope){
-    
+app.controller("outletEmployeeFormController", [
+  "$scope",
+  "$http",
+  "$location",
+  "apiHandler",
+  "outletApi",
+  "$rootScope",
+  function ($scope, $http, $location, apiHandler, outletApi, $rootScope) {
     // outletApi.getOutletAdminPage(function(err,result){
     //     if(result){
     //         console.log(result.data);
     //         $scope.outletData=result.data.outletData;
     //     }
-        
-        
+
     // });
-    $rootScope.$on('passData',function(err,data){
-        if(data){
-            $scope.outletData=data.data.outletData;
-                
-        }
+    $rootScope.$on("passData", function (err, data) {
+      if (data) {
+        $scope.outletData = data.data.outletData;
+      }
     });
 
-    $scope.btnText="create agent";
-    $scope.agent={};
+    $scope.btnText = "create agent";
+    $scope.agent = {};
 
-    $scope.createOutletAgent=function($event,brandId){
-        $event.preventDefault();
-        $scope.btnText="creating...";
-       
-        outletApi.createOutletAgent({...$scope.agent,brand:$scope.outletData.brand,outlet:{_id:$scope.outletData._id,type:$scope.outletData.type,name:$scope.outletData.name}},function(err,result){
-            if(result){
-                console.log(result);
-                $scope.btnText="created";
-            }
-        })
-    }
-    
+    $scope.createOutletAgent = function ($event) {
+      $event.preventDefault();
+      $scope.btnText = "creating...";
 
-
-}])
+      outletApi.createOutletAgent(
+        {
+          ...$scope.agent,
+          brand: $scope.outletData.brand,
+          outlet: {
+            _id: $scope.outletData._id,
+            type: $scope.outletData.type,
+            name: $scope.outletData.name,
+          },
+        },
+        function (err, result) {
+          if (result) {
+            console.log(result);
+            $scope.btnText = "created";
+            $("#exampleModal").modal("hide");
+          }
+        }
+      );
+    };
+  },
+]);
